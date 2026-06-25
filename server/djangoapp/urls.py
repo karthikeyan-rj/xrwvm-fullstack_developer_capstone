c3d65761300e19f9561bb3e9be94d472f8ceee6d
Karthikeyan R J <karthikeyanrj@student.tce.edu>
2026-06-25 14:54:22 -0400

    Add car make and car model models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
     path(route='register', view=views.registration, name='register'),
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -117,3 +117,23 @@ def registration(request):
         return JsonResponse({"userName": username, "status": "Registered"})
 
     return JsonResponse({"status": "Invalid request"})
+
+
+from .models import CarMake, CarModel
+from .populate import initiate
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
